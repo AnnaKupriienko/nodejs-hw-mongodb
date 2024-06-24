a1f316ab0d846bc36c8f6ece9573e79464955e96
Anna <kupriienko16@gmail.com>
2024-06-24 16:46:44 +0100

    commit

---
+++ src/server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import { env } from './utils/env.js';
+
+const PORT = Number(env('PORT', '3000'));
+
+const setupServer = () => {
+    const app =express();
+
+    app.use(express.json());
+    app.use(cors());
+
+    app.use(
+        pino({
+            transport: {
+                target: 'pino-pretty',
+            },
+        }),
+    );
+    app.use('*', (req, res, next) => {
+        res.status(404).json({
+            message: 'Not found',
+        });
+    });
+
+    app.listen(PORT, () => { console.log(`Server is running on port $npm{PORT}`) })
+}
+
+export default setupServer;
