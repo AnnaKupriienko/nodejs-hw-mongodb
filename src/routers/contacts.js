e4f7f4de593c85d91382a635ac24fd131083453b
Anna <kupriienko16@gmail.com>
2024-07-02 14:11:27 +0100

    first commit

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -7,14 +7,17 @@ import {
   deleteContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { createContactSchema, updateContactSchema} from '../validation/contacts.js';
+
 
 const router = Router();
 
 export default router;
-router.get('/contacts', ctrlWrapper(getContactsController));
-router.get('/contacts/:contactId', ctrlWrapper(getContactsByIdController));
-router.post('/contacts', ctrlWrapper(createContactController));
-router.patch('/contacts/:contactId', ctrlWrapper(updateContactController));
+router.get('/contacts',ctrlWrapper(getContactsController));
+router.get('/contacts/:contactId', isValidId(), ctrlWrapper(getContactsByIdController));
+router.post('/contacts', validateBody(createContactSchema), ctrlWrapper(createContactController));
+router.patch('/contacts/:contactId',validateBody(updateContactSchema), ctrlWrapper(updateContactController));
 router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
 
 
